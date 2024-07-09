b3fd5e3c0494c4220d88177a0a863bc3a0a4ffcb
Frits-v2 <46125@hoornbeeck.nl>
2024-07-09 23:15:54 +0200

    commit

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,36 +1,33 @@
 /* eslint-disable no-restricted-globals */
-// Import necessary Workbox modules
 import { precacheAndRoute } from 'workbox-precaching';
 
 // Initialize self.__WB_MANIFEST if not already defined
-self.__WB_MANIFEST = [].concat(self.__WB_MANIFEST || []);
+self.__WB_MANIFEST = self.__WB_MANIFEST || [];
 
 // Precache routes using Workbox
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Other service worker configurations and caching strategies can follow here
+// Service worker install event
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('static-v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/logo192.png',
-          '/logo512.png',
-          // Add other static assets that need to be cached
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('static-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/logo192.png',
+        '/logo512.png',
+        // Add other static assets that need to be cached
+      ]);
+    })
+  );
+});
 
-
-
+// Service worker fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
